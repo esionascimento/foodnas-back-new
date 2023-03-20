641b787c97dec6034b22b1edaaf54f41e0538098
esionascimento <esionascimento@proton.me>
2023-03-20 00:11:12 -0400

    feat: auth.controller

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateAuthDto } from './dto/create-auth.dto';
+import { UpdateAuthDto } from './dto/update-auth.dto';
+import { Usuario } from 'src/entities/usuario.entity';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('/login')
+  async createUser(
+    @Body('password') password: string,
+    @Body('username') username: string,
+  ): Promise<Usuario> {
+    // const hashedPassword = await bcrypt.hash(password, saltOrRounds);
+    // const result = await this.usersService.createUser(
+    //     username,
+    //     hashedPassword,
+    // );
+    return;
+  }
+}
