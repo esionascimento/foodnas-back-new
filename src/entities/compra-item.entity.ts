736a0e363af1ba87013cbf1ac160af04e4dbb32c
esionascimento <esionascimento@proton.me>
2023-03-21 02:43:59 -0400

    feat: compra-item.entity.ts

---
+++ src/entities/compra-item.entity.ts
@@ -0,0 +1,26 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { Compra } from './compra.entity';
+import { Lote } from './lote.entity';
+import { Usuario } from './usuario.entity';
+import { BaseEntity } from './base.entity';
+
+@Entity()
+export class ItensCompra extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne((type) => Compra, (compra) => compra.itensCompra)
+  compra: Compra;
+
+  @ManyToOne((type) => Lote, (lote) => lote.itensCompra)
+  lote: Lote;
+
+  @Column()
+  quantidade: number;
+
+  @Column()
+  valor_unitario: number;
+
+  @ManyToOne((type) => Usuario, (usuario) => usuario.itensCompra)
+  usuario: Usuario;
+}
