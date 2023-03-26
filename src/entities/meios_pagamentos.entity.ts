5ddc67967c67764ba557ba7931fc4597bd8ba430
esionascimento <esionascimento@proton.me>
2023-03-26 04:12:29 -0400

    feat: meio_pagamentos.entity

---
+++ src/entities/meios_pagamentos.entity.ts
@@ -0,0 +1,15 @@
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Venda } from "./vendas.entity";
+import { BaseEntity } from "./base.entity";
+
+@Entity()
+export class MeioPagamentos extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 100 })
+  descricao: string;
+
+  @ManyToMany(() => Venda, (venda) => venda.meiosPagamento)
+  vendas: Venda[];
+}
