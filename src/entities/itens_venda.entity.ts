78e158fc7f3c9bb43ded6986b045ccbe5ffdf35e
esionascimento <esionascimento@proton.me>
2023-03-25 23:48:17 -0400

    rename: arquivo itens_venda.entity

---
+++ src/entities/itens_venda.entity.ts
@@ -0,0 +1,24 @@
+import { BaseEntity, Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Venda } from './vendas.entity';
+import { Lote } from './lote.entity';
+import { Usuario } from './usuarios.entity';
+
+@Entity()
+export class VendaItem extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'integer' })
+  quantidade: number;
+  
+  @Column({ name: 'valor_unitario', type: 'decimal', precision: 10, scale: 2 })
+  valorUnitario: number;
+  
+  @ManyToOne(() => Venda, (venda) => venda.itens)
+  @JoinColumn({ name: 'id_venda' })
+  venda: Venda;
+  
+  @ManyToOne(() => Lote)
+  @JoinColumn({ name: 'id_lote' })
+  lote: Lote;
+}
