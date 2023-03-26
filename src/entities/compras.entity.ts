af6d5f7dd6471860da7fc2223193513a30ef570e
esionascimento <esionascimento@proton.me>
2023-03-25 23:41:30 -0400

    refactor: compras.entity @Column type, name, enum @JoinColumn

--- src/entities/compras.entity.ts
+++ src/entities/compras.entity.ts
@@ -1,5 +1,12 @@
-import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, OneToMany } from 'typeorm';
-import { Usuario } from './usuario.entity';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  OneToMany,
+  JoinColumn,
+} from 'typeorm';
+import { Usuario } from './usuarios.entity';
 import { ItensCompra } from './itens_compra.entity';
 
 @Entity()
@@ -7,18 +14,19 @@ export class Compra {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: 'date' })
+  @Column('datetime', { name: 'data_compra' })
   dataCompra: Date;
 
-  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  @Column('decimal', { name: 'valor_total', precision: 10, scale: 2 })
   valorTotal: number;
 
-  @OneToMany(type => ItensCompra, itensCompra => itensCompra.compra)
+  @Column('enum', { enum: ['PENDENTE', 'CONCLUIDA', 'CANCELADA'] })
+  status: string;
+
+  @OneToMany((type) => ItensCompra, (itensCompra) => itensCompra.compra)
   itensCompra: ItensCompra[];
 
-  @Column()
-  id_usuario: number;
-
   @ManyToOne(() => Usuario)
+  @JoinColumn({ name: 'id_usuario' })
   usuario: Usuario;
 }
