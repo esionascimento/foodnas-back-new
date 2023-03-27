e60e6e1933290262d0c71d0ff0d83afb22a4bc81
esionascimento <esionascimento@proton.me>
2023-03-26 20:42:18 -0400

    refactor: itens_orcamento.entity add relacionamento orcamento, produto

--- src/entities/itens_orcamento.entity.ts
+++ src/entities/itens_orcamento.entity.ts
@@ -1,8 +1,16 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Orcamento } from './orcamentos.entity';
+import { Produto } from './produtos.entity';
+import { BaseEntity } from './base.entity';
 
 @Entity('itens_orcamento')
-export class ItensOrcamento {
+export class ItensOrcamento extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
@@ -12,5 +20,11 @@ export class ItensOrcamento {
   @Column('decimal', { name: 'valor_unitario', precision: 10, scale: 2 })
   valorUnitario: number;
 
-  
+  @ManyToOne(() => Orcamento)
+  @JoinColumn({ name: 'id_orcamento' })
+  orcamento: Orcamento;
+
+  @ManyToOne(() => Produto)
+  @JoinColumn({ name: 'id_produto' })
+  produto: Produto;
 }
