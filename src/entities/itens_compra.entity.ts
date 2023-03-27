84cb4f13dc5389b392bc0549a7b013cd5fa3ea9a
esionascimento <esionascimento@proton.me>
2023-03-26 20:41:40 -0400

    refactor: itens_compra.entity types columns, add JoinColumn, add names columns

--- src/entities/itens_compra.entity.ts
+++ src/entities/itens_compra.entity.ts
@@ -1,7 +1,12 @@
-import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
-import { Compra } from './compras.entity';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { Compras } from './compras.entity';
 import { Lote } from './lote.entity';
-import { Usuario } from './usuario.entity';
 import { BaseEntity } from './base.entity';
 
 @Entity()
@@ -9,18 +14,17 @@ export class ItensCompra extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @ManyToOne((type) => Compra, (compra) => compra.itensCompra)
-  compra: Compra;
-
-  @ManyToOne((type) => Lote, (lote) => lote.itensCompra)
-  lote: Lote;
-
-  @Column()
+  @Column('integer')
   quantidade: number;
 
-  @Column()
-  valor_unitario: number;
+  @Column('decimal', { name: 'valor_unitario', precision: 10, scale: 2 })
+  valorUnitario: number;
 
-  @ManyToOne((type) => Usuario, (usuario) => usuario.itensCompra)
-  usuario: Usuario;
+  @ManyToOne((type) => Compras, (compra) => compra.itensCompra)
+  @JoinColumn({ name: 'id_compra' })
+  compra: Compras;
+
+  @ManyToOne((type) => Lote, (lote) => lote.itensCompra)
+  @JoinColumn({ name: 'id_lote' })
+  lote: Lote;
 }
