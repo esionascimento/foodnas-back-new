c907778d0fecfe35f461da4406af538f7f62ad06
esionascimento <esionascimento@proton.me>
2023-04-02 18:52:03 -0400

    refactor: main add enableCors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const PORT = process.env.PORT;
+  const PORT = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
+  // app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors();
   await app.listen(PORT);
   console.info('aplicação rodando na porta: ' + PORT);
 }
