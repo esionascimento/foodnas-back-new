60b22f81f719cf3f29f015dc8c755ba84af67a60
esionascimento <esionascimento@proton.me>
2023-04-02 18:58:29 -0400

    refactor: app.module add GraphQLModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './modules/usuarios/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from './modules/auth/auth.module';
 import { dataSourceOptions } from './db/data-source';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloDriver } from '@nestjs/apollo/dist/drivers';
+import { UserResolver } from '@graphql/resolver/user.resolver';
+import { AuthModule } from '@modules/auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
     TypeOrmModule.forRoot(dataSourceOptions),
     UsersModule,
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [UserResolver],
 })
 export class AppModule {}
