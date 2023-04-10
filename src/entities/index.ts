df83e89672d0508bae683c48a44e37e7ad5cbafd
esionascimento <esionascimento@proton.me>
2023-04-09 22:23:35 -0400

    feat: entities index

---
+++ src/entities/index.ts
@@ -0,0 +1,37 @@
+import { BaseEntity } from './base.entity';
+import { Compras } from './compras.entity';
+import { Estoque } from './estoque.entity';
+import { Fornecedores } from './fornecedores.entity';
+import { ItensCompra } from './itens_compra.entity';
+import { ItensOrcamento } from './itens_orcamento.entity';
+import { VendaItem } from './itens_venda.entity';
+import { Lote } from './lote.entity';
+import { MeioPagamentos } from './meios_pagamentos.entity';
+import { MovimentacoesEstoque } from './movimentacoes_estoque.entity';
+import { Orcamento } from './orcamentos.entity';
+import { Preco } from './precos.entity';
+import { Produto } from './produtos.entity';
+import { Usuario } from './usuarios.entity';
+import { Venda } from './vendas.entity';
+import { VendasMeioPagamento } from './vendas_meios_pagamentos.entity';
+
+const entities = [
+  BaseEntity,
+  Usuario,
+  Fornecedores,
+  Produto,
+  Compras,
+  Estoque,
+  ItensCompra,
+  ItensOrcamento,
+  VendaItem,
+  Lote,
+  MeioPagamentos,
+  MovimentacoesEstoque,
+  Orcamento,
+  Preco,
+  VendasMeioPagamento,
+  Venda,
+];
+
+export default entities;
