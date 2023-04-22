aca67386800cee32896b11e9199fd168bce2aa6f
esionascimento <esionascimento@proton.me>
2023-04-21 21:30:57 -0400

    refactor: app.module impport AuthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,8 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriverConfig } from '@nestjs/apollo';
 import { ApolloDriver } from '@nestjs/apollo/dist/drivers';
 import { UserResolver } from '@graphql/resolver/user.resolver';
-import { AuthModule } from '@modules/auth/auth.module';
 import { dataSourceOptions } from './database/ormconfig';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
