3ac8b2d992fee773d8f160a77aff670a476d9739
esionascimento <esionascimento@proton.me>
2023-05-02 09:02:50 -0400

    refactor: usuarios.entity roles, lojas

--- src/entities/usuarios.entity.ts
+++ src/entities/usuarios.entity.ts
@@ -1,7 +1,15 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
-import { Produto } from './produtos.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToMany,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Produtos } from './produtos.entity';
 import { Fornecedores } from './fornecedores.entity';
-import { Lote } from './lote.entity';
+import { Lotes } from './lote.entity';
 import { Preco } from './precos.entity';
 import { Estoque } from './estoque.entity';
 import { Venda } from './vendas.entity';
@@ -9,6 +17,9 @@ import { Compras } from './compras.entity';
 import { Orcamento } from './orcamentos.entity';
 import { BaseEntity } from './base.entity';
 import { Field, ObjectType } from '@nestjs/graphql';
+import { EPermission, TPermissionType } from './enum';
+import { Lojas } from './lojas.entity';
+import { Permissoes } from './permissoes.entity';
 
 @ObjectType()
 @Entity()
@@ -21,21 +32,31 @@ export class Usuarios extends BaseEntity {
   @Column('varchar', { length: 50 })
   nome: string;
 
+  @Field()
   @Column('varchar', { length: 255 })
   email: string;
 
-  @Field()
   @Column('varchar', { length: 255 })
   senha: string;
 
-  @OneToMany(() => Produto, (produto) => produto.usuario)
-  produtos: Produto[];
+  @Column({ type: 'enum', enum: EPermission })
+  roles: TPermissionType;
+
+  @ManyToOne(() => Lojas, (loja) => loja.usuario)
+  @JoinColumn({ name: 'id_loja', referencedColumnName: 'id' })
+  loja: Lojas;
+
+  @ManyToMany(() => Permissoes, (permissoes) => permissoes.usuarios)
+  permissoes: Permissoes[];
+
+  @OneToMany(() => Produtos, (produto) => produto.usuario)
+  produtos: Produtos[];
 
   @OneToMany(() => Fornecedores, (fornecedor) => fornecedor.usuario)
   fornecedores: Fornecedores[];
 
-  @OneToMany(() => Lote, (lote) => lote.usuario)
-  lotes: Lote[];
+  @OneToMany(() => Lotes, (lote) => lote.usuario)
+  lotes: Lotes[];
 
   @OneToMany(() => Preco, (preco) => preco.usuario)
   precos: Preco[];
