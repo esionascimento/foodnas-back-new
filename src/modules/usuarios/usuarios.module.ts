9e3ca4fcf2ab4272e6cdbf54e2da7b45369ae5ad
esionascimento <esionascimento@proton.me>
2023-05-03 11:33:53 -0400

    refactor: usuarios.module import Lojas entity

--- src/modules/usuarios/usuarios.module.ts
+++ src/modules/usuarios/usuarios.module.ts
@@ -3,9 +3,10 @@ import { UsuariosService } from './usuarios.service';
 import { UsuariosResolver } from './usuarios.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Usuarios } from '@/entities/usuarios.entity';
+import { Lojas } from '@/entities/lojas.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Usuarios])],
+  imports: [TypeOrmModule.forFeature([Usuarios, Lojas])],
   providers: [UsuariosService, UsuariosResolver],
   exports: [UsuariosService],
 })
