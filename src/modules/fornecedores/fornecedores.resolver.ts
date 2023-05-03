adfa514588c110a541ece1b634d27b7f5d00d474
esionascimento <esionascimento@proton.me>
2023-05-03 11:35:14 -0400

    refactor: fornecedores.resolver UseGuards, context CreateFornecedor

--- src/modules/fornecedores/fornecedores.resolver.ts
+++ src/modules/fornecedores/fornecedores.resolver.ts
@@ -1,17 +1,21 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { GetFornecedorArgs } from './dto/get-fornecedor-args';
 import { FornecedoresService } from './fornecedores.service';
 import { CreateOneFornecedoresInput } from './dto/inputs/create-fornecedores.input';
 import { Fornecedores } from '@/entities/fornecedores.entity';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { UseGuards } from '@nestjs/common';
 
 @Resolver()
 export class FornecedoresResolver {
   constructor(private fornecedoresService: FornecedoresService) {}
 
   @Mutation(() => Fornecedores, { name: 'CreateFornecedor' })
+  @UseGuards(JwtAuthGuard)
   async CreateFornecedor(
     @Args('input') input: CreateOneFornecedoresInput,
+    @Context() context,
   ): Promise<Fornecedores> {
-    return this.fornecedoresService.create(input);
+    return this.fornecedoresService.create(input, context.req.user);
   }
 }
