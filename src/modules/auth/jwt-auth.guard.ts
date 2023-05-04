19776a36792f2ee3f06e9082a31708434f3812e8
esionascimento <esionascimento@proton.me>
2023-05-04 09:06:53 -0400

    refactor: jwt-auth.guard canActivate ignore resolver

--- src/modules/auth/jwt-auth.guard.ts
+++ src/modules/auth/jwt-auth.guard.ts
@@ -1,11 +1,32 @@
 import { Injectable, ExecutionContext } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
   getRequest(context: ExecutionContext) {
     const ctx = GqlExecutionContext.create(context);
     return ctx.getContext().req;
+  }
+
+  canActivate(context: ExecutionContext) {
+    const ctx = GqlExecutionContext.create(context);
+    if (ctx.getHandler().name === 'login') {
+      return true;
+    }
+
+    const ignoredModules = this.reflector.get<string[]>(
+      'ignoredModules',
+      context.getHandler(),
+    );
+    if (ignoredModules && ignoredModules.includes(context.getClass().name)) {
+      return true;
+    }
+    return super.canActivate(context);
   }
 }
