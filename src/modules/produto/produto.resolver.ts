88950dd9d288f2483176a56713b3ec5fa78e7003
esionascimento <esionascimento@proton.me>
2023-05-10 22:22:07 -0400

    feat: produto.resolver CreateProduto

---
+++ src/modules/produto/produto.resolver.ts
@@ -0,0 +1,17 @@
+import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { ProdutoService } from './produto.service';
+import { CreateOneProdutoInput } from './dto/inputs/create-produto.input';
+import { Produtos } from '@/entities/produtos.entity';
+
+@Resolver()
+export class ProdutoResolver {
+  constructor(private produtoService: ProdutoService) {}
+
+  @Mutation(() => Produtos)
+  async CreateProduto(
+    @Args('input') input: CreateOneProdutoInput,
+    @Context() context,
+  ): Promise<Produtos> {
+    return this.produtoService.create(input, context.req.user);
+  }
+}
