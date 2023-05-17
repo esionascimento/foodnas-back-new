5a0bbb7fb377aa049e5ac38db4fb5924780c1be5
esionascimento <esionascimento@proton.me>
2023-05-17 19:22:12 -0400

    refactor: create-produto.input InputType, CaracteristicaInput

--- src/modules/produto/dto/inputs/create-produto.input.ts
+++ src/modules/produto/dto/inputs/create-produto.input.ts
@@ -1,6 +1,21 @@
 import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
+class CreateProductTipo {
+  @Field()
+  nome: string;
+}
+
+@InputType()
+class CreateProductCaracteristica {
+  @Field()
+  nome: string;
+
+  @Field()
+  valor: string;
+}
+
+@InputType()
 export class CreateOneProdutoInput {
   @Field()
   nome: string;
@@ -10,4 +25,37 @@ export class CreateOneProdutoInput {
 
   @Field()
   unidade: string;
+
+  @Field(() => CreateProductTipo)
+  tipo: CreateProductTipo;
+
+  @Field(() => CreateProductCaracteristica)
+  caracteristica: CreateProductCaracteristica;
+}
+
+@InputType()
+class CaracteristicaInput {
+  @Field()
+  nome: string;
+
+  @Field()
+  valor: string;
+}
+
+@InputType()
+export class ProdutoInput {
+  @Field()
+  nome: string;
+
+  @Field()
+  descricao: string;
+
+  @Field()
+  unidade: string;
+
+  @Field()
+  tipoId: number;
+
+  @Field(() => [CaracteristicaInput])
+  caracteristicas: CaracteristicaInput[];
 }
