1c9a05745afcc8e73553191ffb1b4735ea827490
esionascimento <esionascimento@proton.me>
2023-05-20 22:18:47 -0400

    refactor: ormconfig novas configs

--- src/database/ormconfig.ts
+++ src/database/ormconfig.ts
@@ -3,22 +3,22 @@ import entities from '@/entities';
 import 'dotenv/config';
 
 export const dataSourceOptions: DataSourceOptions = {
-  url: process.env.DATABASE_URL,
+  // url: process.env.DATABASE_URL,
   type: 'mysql',
-  // host: process.env.DATABASE_HOST,
-  // port: parseInt(process.env.DATABASE_PORT, 10) || 3306,
-  // username: process.env.DATABASE_USERNAME,
-  // password: process.env.DATABASE_PASSWORD,
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT, 10) || 3306,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_DATABASE,
   entities: entities,
   migrations: ['dist/database/migrations/*{.ts,.js}'],
   synchronize: false,
-  logging: true,
-  extra: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
+  logging: false,
+  // extra: {
+  //   ssl: {
+  //     rejectUnauthorized: false,
+  //   },
+  // },
 };
 const dataSource = new DataSource(dataSourceOptions);
 
