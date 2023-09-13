70695b8b38ec5ced63538d1cbcbbd74343678eb8
esionascimento <esionascimento@proton.me>
2023-09-13 11:44:48 -0400

    refactor: produto.module add Caracteristicas e Tipos imports

--- src/modules/produto/produto.module.ts
+++ src/modules/produto/produto.module.ts
@@ -5,10 +5,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Produtos } from '@/entities/produtos.entity';
 import { Roles } from '@/entities/roles.entity';
 import { Usuarios } from '@/entities/usuarios.entity';
+import { Caracteristicas } from '@/entities/caracteristicas.entity';
+import { Tipos } from '@/entities/tipos.entity';
 
 @Module({
   controllers: [],
-  imports: [TypeOrmModule.forFeature([Produtos, Roles, Usuarios])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Produtos,
+      Roles,
+      Usuarios,
+      Caracteristicas,
+      Tipos,
+    ]),
+  ],
   providers: [ProdutoService, ProdutoResolver],
   exports: [ProdutoService],
 })
